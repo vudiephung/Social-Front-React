ad584a47eb843a66b133be86c34d57e7a875a8eb
vudiephung <hungvudiep@gmail.com>
2019-11-22 13:23:31 +0700

    Updates

---
+++ precache-manifest.871cd863f3ab494ca38380b081c3e55d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2b8775d2a95f6c320acf9332850ac09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92d545ce16cbf66b801a",
+    "url": "/static/js/2.b246b247.chunk.js"
+  },
+  {
+    "revision": "757d5727b706c4015f1e",
+    "url": "/static/js/main.784926b8.chunk.js"
+  },
+  {
+    "revision": "67e158595d5cd94da31c",
+    "url": "/static/js/runtime-main.52f75924.js"
+  },
+  {
+    "revision": "046e91f77483a277a434bac9ae4885e2",
+    "url": "/static/media/avatar.046e91f7.jpg"
+  },
+  {
+    "revision": "33f2c94149baa62dd02d6fd51016e6bb",
+    "url": "/static/media/post.33f2c941.jpg"
+  }
+]);
